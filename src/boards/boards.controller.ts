80a11544180afa33ab0fbacdf9bab1cf8fe2280a
golbangi1000 <kenjames777@naver.com>
2023-11-20 21:52:24 +0900

    awfe
    awefafwe

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+
+export class BoardsController {
+    //pirvate을 쓰면 암묵적으로 프로퍼티로 선언이됨
+    constructor(private boardsService: BoardsService){}
+
+
+    // GET POST PUT DELETE 이 있는데 57:39
+    return this.boardsService.getAllBoards
+}
+
